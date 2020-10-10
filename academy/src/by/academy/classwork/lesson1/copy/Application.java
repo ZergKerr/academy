4d10f0209f0a8d4306c9da54a9596ae4dc002995
Sergey Shishko <sshishko@gmail.com>
2020-10-10 15:22:41 +0300

    After lesson 2

---
+++ academy/src/by/academy/classwork/lesson1/copy/Application.java
@@ -0,0 +1,26 @@
+package by.academy.classwork.lesson1.copy;
+
+public class Application {
+	
+	public static void main(String ...args){
+		Cat unnamed = new Cat();
+		Cat pushok = new Cat("Pushok");
+		Cat pushok1 = new Cat("Bug", 3);
+		
+		pushok.eat();
+		pushok.sleep();
+		pushok.walk();
+		
+		unnamed.grow(2);
+		unnamed.grow(3);
+		unnamed.grow(4);
+		
+		System.out.println("Unnamed cat`s age is equal to " + unnamed.age);
+		//System.out.println("Bug`s age is equal to " + pushok1.age);
+		pushok1.grow(5);
+		
+		pushok.setMoney(105);
+		
+		System.out.println(pushok.getMoney());
+	}
+}
---
+++ academy/src/by/academy/classwork/lesson1/copy/Cat.java
@@ -0,0 +1,49 @@
+package by.academy.classwork.lesson1.copy;
+
+public class Cat {
+	public int age;
+	public String nickname;
+	double money;
+
+	public Cat() {
+
+	}
+
+	public Cat(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public Cat(String nickname, int age) {
+		this.nickname = nickname;
+		this.age = age;
+	}
+
+	public void setMoney(double money) {
+		this.money = money;
+	}
+
+	public double getMoney() {
+		return money;
+	}
+
+	public void grow(int addAge) {
+		System.out.println("Кот вырос на " + addAge + " года");
+		age = age + addAge;
+	}
+
+	public void sleep() {
+		System.out.println(this.nickname + " sleeping");
+	}
+
+	public void eat() {
+		System.out.println(this.nickname + " eating");
+	}
+
+	public void walk() {
+		System.out.println(this.nickname + " walking");
+	}
+
+	public int getAge() {
+		return age;
+	}
+}
